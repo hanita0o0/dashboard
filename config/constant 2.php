059df098e739e19d6550f97aa5ddc21e92625b7f
hanita0o0 <40276066+hanita0o0@users.noreply.github.com>
2018-11-24 15:50:30 +0330

    add constant2.php file in config folder

---
+++ config/constant 2.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shahla
+ * Date: 9/22/2018
+ * Time: 12:36 PM
+ */
+return[
+    'user_avatar'=>'http://st.mazetrip.ir/users/avatar/thumbnail',
+    'noImageUser'=>'/nouser.jpg',
+    'path_user_avatar1'=>'/home/mazetrip/backend/storage/users/avatar/original/',
+    'path_user_avatar2'=>'/home/mazetrip/backend/storage/users/avatar/thumbnail/',
+    'path_user_avatar3'=>'/home/mazetrip/backend/storage/users/avatar/medium/',
+    'path_user_avatar4'=>'/home/mazetrip/backend/storage/users/avatar/large/',
+    
+    'club_avatar'=>'http://st.mazetrip.ir/photos/events/event_profile/thumbnail',
+    'noImageClub'=>'/noclub.png',
+    'path_club_avatar'=>'/home/mazetrip/backend/storage/',
+    'path_club_avatar1'=>'/home/mazetrip/backend/storage/photos/events/event_profile/original/',
+    'path_club_avatar2'=>'/home/mazetrip/backend/storage/photos/events/event_profile/thumbnail/',
+    'path_club_avatar3'=>'/home/mazetrip/backend/storage/photos/events/event_profile/medium/',
+  
+    
+    'post_avatar'=>'http://st.mazetrip.ir/photos/posts/thumbnail',
+    'noImagePost'=>'/nopost.jpg',
+    'path_post_avatar1'=>'/home/mazetrip/backend/storage/photos/posts/original/',
+    'path_post_avatar2'=>'/home/mazetrip/backend/storage/photos/posts/thumbnail/',
+    'path_post_avatar3'=>'/home/mazetrip/backend/storage/photos/posts/medium/',
+    'path_post_avatar'=>'/home/mazetrip/backend/storage/',
+    
+    'type_avatar'=>'http://st.mazetrip.ir/photos/types/thumbnail',
+    'path_type_avatar'=>'/home/mazetrip/backend/storage/photos/types/thumbnail/',
+    'noImageType'=>'/notype.jpg',
+    
+   
+    'ticket_avatar'=>'http://st.mazetrip.ir/photos/events/ticket/thumbnail',
+    'noImageTicket'=>'/noticket.jpg',
+   
+
+];
